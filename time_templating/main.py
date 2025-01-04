bb9529b5dbe3590a8603b2ef45ded8fa206364f8
misha <medinskijmisa228@gmail.com>
2025-01-04 21:19:03 +0200

    add main.py

---
+++ time_templating/main.py
@@ -0,0 +1,16 @@
+from datetime import date
+from utils import is_execution_time
+
+
+def check_schedule(date_day: date, code: str) -> bool:
+    code_components: list[str] = code.split("_&_")
+    need_todo: list[bool] = []
+    for component in code_components:
+        need_todo.append(is_execution_time(date_day=date_day, component=component))
+    return True if True in need_todo else False
+
+
+if __name__ == "__main__":
+    now_date = date(year=2024, month=1, day=1)
+    component = "e_w_mo_&_e_w_tu"
+    print(check_schedule(date_day=now_date, code=component))
