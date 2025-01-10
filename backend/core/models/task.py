d5d1d28b2a2f94a3fb0c75bfeb258d8a07af84ad
misha <medinskijmisa228@gmail.com>
2025-01-10 15:30:30 +0200

    black lint

--- backend/core/models/task.py
+++ backend/core/models/task.py
@@ -2,12 +2,11 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey
 from .base import Base
 
+
 class Task(Base):
     name: Mapped[str]
     description: Mapped[str]
     is_complete: Mapped[bool]
-    
-    
-    
+
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
     user: Mapped["User"] = relationship(back_populates="tasks")
