a89bc9452812559ea0fffed5aa49a6f9527bf493
misha <medinskijmisa228@gmail.com>
2025-01-10 13:32:51 +0200

    add user model

---
+++ backend/core/models/user.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
+from .base import Base
+
+
+class User(Base):
+    user_name: Mapped[str]
+    chat_id: Mapped[str] = mapped_column(unique=True)
+    tasks: Mapped[List["Task"]] = relationship(
+        back_populates="user", uselist=True, lazy="selectin"
+    )
