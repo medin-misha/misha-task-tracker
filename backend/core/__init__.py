5822575871952d61f98308aeefa6028241ab71e0
misha <medinskijmisa228@gmail.com>
2025-01-13 13:52:45 +0200

    export db_hepler

--- backend/core/__init__.py
+++ backend/core/__init__.py
@@ -1,3 +1,5 @@
-__all__ = ("settings",)
+__all__ = ("settings", "create", "get_by_id", "get_list", "delete", "update")
 
 from .config import settings
+from .crud import create, get_by_id, get_list, delete, update
+from .db_helper import db_helper
