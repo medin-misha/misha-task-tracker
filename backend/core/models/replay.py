2c7cb97d673d8a9d8a72a5f2e25209d722670017
misha <medinskijmisa228@gmail.com>
2025-02-10 13:10:56 +0200

    delete replay.how_many

--- backend/core/models/replay.py
+++ backend/core/models/replay.py
@@ -6,10 +6,10 @@ from .base import Base
 
 class Replay(Base):
     counter: Mapped[int] = mapped_column(default=0, server_default="0")
-    how_many: Mapped[int] = mapped_column(nullable=True)
     date: Mapped[str] = mapped_column(Date, nullable=True)
     time: Mapped[str] = mapped_column(
         Time, default=datetime.now().time, server_default=func.current_time()
     )
     replay_mode: Mapped[str] = mapped_column(nullable=True)
     task: Mapped["Task"] = relationship(back_populates="replay")
+    
