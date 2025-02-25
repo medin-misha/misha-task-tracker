48713890947bfb0fb9db00ac8ff6f326660a5554
misha <medinskijmisa228@gmail.com>
2025-02-25 14:18:49 +0200

    add registraion, login, base_encode functions

---
+++ bot/utils/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ("registration", "login")
+
+from .auth import registration, login
---
+++ bot/utils/auth.py
@@ -0,0 +1,31 @@
+import aiohttp
+import json
+from typing import Dict
+import base64
+from settings import config
+
+def base_encode(user_name: str, user_id: int) -> str:
+        string: str = f"{user_name}:{user_id}"
+        str_bytes: bytes = string.encode("ascii")
+        str_base64: str = base64.b64encode(str_bytes).decode("ascii")
+        print(str_base64)
+        return str_base64
+
+async def registration(user_name: str, user_id: int) -> int:
+    async with aiohttp.ClientSession() as session:
+        data: Dict[str, str] = {
+            "user_name": user_name,
+            "chat_id": str(user_id),
+        }
+        async with session.post(
+            url=config.api_address + "auth/registration", json=data
+        ) as response:
+            return response.status
+
+
+async def login(user_name: str, user_id: int) -> int:
+    async with aiohttp.ClientSession() as session:
+        auth_base64 = base_encode(user_name=user_name, user_id=user_id)
+        headers: Dict[str, str] = {"Authorization": f"Basic TnVhbE53c3ViOjU4MjgzMjY0ODI="}
+        async with session.get(url=config.api_address + "auth/login", headers=headers) as response:
+            return response.status
