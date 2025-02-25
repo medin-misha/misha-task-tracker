1844119745b718c7390d5a81eba0fbad1c73af59
misha <medinskijmisa228@gmail.com>
2025-02-25 14:13:23 +0200

    update User model

--- backend/app/core/models/user.py
+++ backend/app/core/models/user.py
@@ -4,7 +4,7 @@ from .base import Base
 
 
 class User(Base):
-    user_name: Mapped[str]
+    user_name: Mapped[str] = mapped_column(unique=True)
     chat_id: Mapped[bytes] = mapped_column(unique=True)
     tasks: Mapped[List["Task"]] = relationship(
         back_populates="user", uselist=True
