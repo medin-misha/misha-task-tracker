d08374ba8f24fc4ef691b84c45c8b429fec96eb7
misha <medinskijmisa228@gmail.com>
2025-02-25 14:22:39 +0200

    update main

--- bot/main.py
+++ bot/main.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher
 
 import asyncio
 import logging
-
+from views import main_router
 from settings import config
 
 
@@ -10,7 +10,7 @@ async def main() -> None:
     logging.basicConfig(level=logging.DEBUG)
     bot = Bot(token=config.token)
     dp = Dispatcher()
-
+    dp.include_router(main_router)
     await dp.start_polling(bot)
 
 
