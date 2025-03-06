23f033ce7a79e8de51e3633193b9efecb52b7d03
misha <medinskijmisa228@gmail.com>
2025-03-06 10:01:15 +0200

    add new keyboards

--- bot/keyboard/__init__.py
+++ bot/keyboard/__init__.py
@@ -1,3 +1,15 @@
-__all__ = ("menu_reply_keyboards",)
+__all__ = (
+    "menu_reply_keyboards",
+    "ButtonsText",
+    "edit_task_data_reply_keyboards",
+    "send_reply_mode_reply_keybpards",
+    "send_data_reply_keyboards",
+)
 
-from .keyboards import menu_reply_keyboards
+from .keyboards import (
+    menu_reply_keyboards,
+    edit_task_data_reply_keyboards,
+    ButtonsText,
+    send_data_reply_keyboards,
+    send_reply_mode_reply_keyboards,
+)
--- bot/keyboard/keyboards.py
+++ bot/keyboard/keyboards.py
@@ -1,13 +1,29 @@
 from aiogram import types
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from datetime import datetime
+from typing import List, Dict
 
 
 class ButtonsText:
     my_today_tasks: str = "мои задачи на сегодня"
-    create_new_task: str = "создать новую задачу"
+    create_new_task: str = "/create"
+    edit_task: Dict[str, str] = {
+        "изменить имя": "name",
+        "изменить описание": "description",
+        "изменить дату": "date",
+        "изменить режим повтора": "reply",
+    }
+    no_edit_task: str = "всё норм"
+    reply_mode: Dict[str, str] = {
+        "один раз": "",
+        "каждый день": "e_d",
+        "каждую неделю": "e_w",
+        "каждый месяц": "e_m",
+    }
 
 
 def menu_reply_keyboards() -> types.ReplyKeyboardMarkup:
-    buttons_line: list[types.KeyboardButton] = [
+    buttons_line: List[types.KeyboardButton] = [
         types.KeyboardButton(text=btn_text)
         for btn_text in [ButtonsText.create_new_task, ButtonsText.my_today_tasks]
     ]
@@ -15,3 +31,32 @@ def menu_reply_keyboards() -> types.ReplyKeyboardMarkup:
         keyboard=[buttons_line]
     )
     return keyboard
+
+
+def edit_task_data_reply_keyboards() -> types.ReplyKeyboardMarkup:
+    edit_line: List[types.KeyboardButton] = [
+        types.KeyboardButton(text=text) for text in ButtonsText.edit_task.keys()
+    ]
+    no_edit_line: List[types.KeyboardButton] = [
+        types.KeyboardButton(text=ButtonsText.no_edit_task)
+    ]
+    keyboard: List[List[types.KeyboardButton]] = [
+        no_edit_line,
+        edit_line[0:2],
+        edit_line[2:4],
+    ]
+    return types.ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
+
+
+def send_data_reply_keyboards() -> types.ReplyKeyboardMarkup:
+    builder = ReplyKeyboardBuilder()
+    builder.button(text=f"{datetime.now().strftime("%Y-%m-%d")}")
+    return builder.as_markup(resize_keyboard=True)
+
+
+def send_reply_mode_reply_keyboards() -> types.ReplyKeyboardMarkup:
+    builder = ReplyKeyboardBuilder()
+    for text in ButtonsText.reply_mode.keys():
+        builder.button(text=text)
+
+    return builder.as_markup(resize_keyboard=True)
