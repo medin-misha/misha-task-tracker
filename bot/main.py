d031cbd19c1fa45942c1005907d9af4576be97bf
misha <medinskijmisa228@gmail.com>
2025-03-06 10:24:07 +0200

    update

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, Dispatcher
-
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 import asyncio
 import logging
 from views import main_router
@@ -8,7 +9,9 @@ from settings import config
 
 async def main() -> None:
     logging.basicConfig(level=logging.DEBUG)
-    bot = Bot(token=config.token)
+    bot = Bot(
+        token=config.token, default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
     dp = Dispatcher()
     dp.include_router(main_router)
     await dp.start_polling(bot)
--- bot/settings.py
+++ bot/settings.py
@@ -9,6 +9,12 @@ class ConfigMessages(BaseSettings):
     help_stiker: str = (
         "CAACAgIAAxkBAAOdZ72xRoTCb57QIgusC3SYvM_QPTQAAiY4AAKDaclKThTMm9A1G0g2BA"
     )
+    what_stiker: str = (
+        "CAACAgIAAxkBAAIBDGfBlsJZr8FvdKlGbZ5XbOrKiM3wAAIDMgACAlLISkI46NLrk3R-NgQ"
+    )
+    star_eye_stiker: str = (
+        "CAACAgIAAxkBAAMyZ72WppzmKXPGej9m9eYr0odwoOwAApMyAAIla8hKHRe9qJyjfek2BA"
+    )
 
 
 class ConfigMain(BaseSettings):
